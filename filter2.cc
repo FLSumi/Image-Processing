482d0387710ce6131398a56d3f13febe7da529e3
!weguez0411 <1066504@my.scccd.edu>
2025-10-28 19:17:30 -0700

    IDK

--- filter2.cc
+++ filter2.cc
@@ -1,4 +1,4 @@
-//Student 2 Name:
+//Student 2 Name: Je-heon Oh
 #include <vector>
 #include <iostream> //IWYU: pragma keep
 #include <iomanip>
@@ -16,20 +16,46 @@ void filter2(vector<vector<vector<int>>> &vec) {
 	size_t colors = vec.at(0).at(0).size();
 	if (!colors) exit(1);
 
+
+
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
 			//DEMO CODE BEGIN
 			//How far are we from the center?
-			const double diag_length = hypot(rows/2.0,cols/2.0); //How many pixels is the diagonal from the center of the image to a corner?
-			double distance = hypot(i-(rows/2.0),j-(cols/2.0));
-			double brightness = 1 - 2*(distance/diag_length); //0 brightness at corners, 200% brightness in middle
+			int r = vec.at(i).at(j).at(RED); //Bounds check with .at the first time
+            int g = vec[i][j][GREEN]; //Skip bounds check for speed
+            int b = vec[i][j][BLUE];
+
+//			const double diag_length = hypot(rows/2.0,cols/2.0); //How many pixels is the diagonal from the center of the image to a corner?
+//			double distance = hypot(i-(rows/2.0),j-(cols/2.0));
+//			double brightness = 1 - 2*(distance/diag_length); //0 brightness at corners, 200% brightness in middle
 			//cout << "i: " << i << " j: " << j << " bright: " << brightness << endl;
 			//cout << "diag_length: " << diag_length << " distance: " << distance << endl;
-			for (int k = 0; k < colors; k++) {
-				vec.at(i).at(j).at(k) *= brightness; //Lighten or darken each color by its distance from the center
-			}
+//			for (int k = 0; k < colors; k++) {
+//				vec.at(i).at(j).at(k) *= brightness; //Lighten or darken each color by its distance from the center
+//			}
 			//DEMO CODE END
-		}
+			if(i < rows/2) {
+            vec[i][j][RED] += 245;
+			vec[i][j][GREEN] += 38;
+            vec[i][j][BLUE] += 38;
+			}
+//			if(j < cols/10){
+			vec.at(i).at(j).at(RED) *= 1.3;
+            vec.at(i).at(j).at(GREEN) *= 1.3;
+            vec.at(i).at(j).at(BLUE) *= 1.3;
+//			}
+			vec.at(i).at(j).at(RED) = g;
+            vec.at(i).at(j).at(GREEN) = b;
+            vec.at(i).at(j).at(BLUE) = r;
+
+/*			if (r > 120 and g > 120) {
+            int avg = (r+g+b)/3;
+            vec[i][j][RED] = avg + 128*cos(i/20.0);
+            vec[i][j][GREEN] = avg + 128*sin(i/50.0);
+            vec[i][j][BLUE] = avg + 128*cos(i/40.0 + 3);
+		}*/
 	}
+}
 }
 
