a2808b67c92991414ed124f2190af84174b0f7a4
salcedo_1026018 <salcedo_1026018@my.scccd.edu>
2025-10-28 21:17:39 -0700

    Finished Filter1

--- filter1.cc
+++ filter1.cc
@@ -9,12 +9,22 @@ const int RED = 0, GREEN = 1, BLUE = 2;
 void make_rect(vector<vector<vector<int>>> &vec, int x_min, int x_max, int y_min, int y_max) {
 	for (int i = y_min; i <= y_max; i++) {
 		for (int j = x_min; j <= x_max; j++) {
-			vec[i][j][RED] = 0;
-			vec[i][j][GREEN] = 0;
-			vec[i][j][BLUE] = 0;
+			vec[i][j][RED] = 90;
+			vec[i][j][GREEN] = 78;
+			vec[i][j][BLUE] = 194;
 		}
 	}
 }
+void make_redrect(vector<vector<vector<int>>> &vec, int x_min, int x_max, int y_min, int y_max) {
+    for (int i = y_min; i <= y_max; i++) {
+        for (int j = x_min; j <= x_max; j++) {
+            vec[i][j][RED] = 500;
+            vec[i][j][GREEN] = 1;
+            vec[i][j][BLUE] = 1;
+        }
+    }
+}
+
 //YOU: Write your own filter here - delete the demo code or modify it to suit your purposes
 //FIXME: There is a fun bug in this code waiting for you to discover
 //Are the cols and rows reversed? It's up to you to find out!
@@ -27,8 +37,10 @@ void filter1(vector<vector<vector<int>>> &vec) {
 	size_t colors = vec.at(0).at(0).size();
 	if (!colors) exit(1);
 
-	make_rect(vec, 50, 200, 50, 60); 
-	make_rect(vec, 50, 200, 100, 110); 
+	make_rect(vec, 1165, 1188, 1692, 1911); 
+	//make_redrect(vec, 1, 15, 4, 40); // starting y, ending y, starting x, ending x
+	//make_redrect(vec, 15, 40 , 4, 10);
+	//make_redrect(vec, 15, 40 , 4, 10);
 
 	//Do the image filtering on every row and column in this image...
 	//Warning: I and J are actuall backwards, I hope you read this comment
@@ -40,30 +52,33 @@ void filter1(vector<vector<vector<int>>> &vec) {
 			int b = vec[i][j][BLUE];
 
 
-			/*
-			//Halftone - every other block of 5x5 pixels is black
+			
+			/*Halftone - every other block of 5x5 pixels is black
 			if ((i/5 + j/5) % 2) {
 				vec.at(i).at(j).at(RED) = 0;
 				vec.at(i).at(j).at(GREEN) = 0;
 				vec.at(i).at(j).at(BLUE) = 0;
 			} */
 			//else {
-				//Color Rotate
-				//vec.at(i).at(j).at(RED) =  g; 
-				//vec.at(i).at(j).at(GREEN) = b;
-				//vec.at(i).at(j).at(BLUE) = r;
+				/*Color Rotate
+				vec.at(i).at(j).at(RED) =  g; 
+				vec.at(i).at(j).at(GREEN) = b;
+				vec.at(i).at(j).at(BLUE) = r;
 			//}*/ 
 			//
 
 			//Filter the middle third of the image
 			if (i > rows/3 and i < (2*rows/3) and j > cols/3 and j < (2*cols/3)) {
-				/*Greyscale
+			/*vec.at(i).at(j).at(RED) *= 0.5;
+            vec.at(i).at(j).at(GREEN) *= 0.5;
+            vec.at(i).at(j).at(BLUE) *= 0.5;
+				*Greyscale
 				int avg = (r+g+b)/3;
 				vec[i][j][RED] = avg;
 				vec[i][j][GREEN] = avg;
 				vec[i][j][BLUE] = avg;
 				*/
-				/* One pixel blur
+				/*One pixel blur
 				int r_avg = (vec[i][j][RED]+vec[i-1][j][RED]+vec[i+1][j][RED]+vec[i][j-1][RED]+vec[i][j+1][RED]) / 5;
 				int g_avg = (vec[i][j][GREEN]+vec[i-1][j][GREEN]+vec[i+1][j][GREEN]+vec[i][j-1][GREEN]+vec[i][j+1][GREEN]) / 5;
 				int b_avg = (vec[i][j][BLUE]+vec[i-1][j][BLUE]+vec[i+1][j][BLUE]+vec[i][j-1][BLUE]+vec[i][j+1][BLUE]) / 5;
@@ -110,13 +125,19 @@ void filter1(vector<vector<vector<int>>> &vec) {
 			//Simple Example - Make the image more yellowish
 			// 
 			vec.at(i).at(j).at(RED) *= 1.8; //Increase red value at every point by 50%
-            vec.at(i).at(j).at(GREEN) *= 1.2; //Increase green at every point by 20%
-            vec.at(i).at(j).at(BLUE) *= 2.2;//Reduce blue by 20%
+            vec.at(i).at(j).at(GREEN) *= 1.1; //Increase green at every point by 20%
+            vec.at(i).at(j).at(BLUE) *= 2.1;//Reduce blue by 20%
 			//
 			vec.at(i).at(j).at(RED) *= 0.6;
             vec.at(i).at(j).at(GREEN) *= 0.6;
             vec.at(i).at(j).at(BLUE) *= 0.6;
 			//
+			if(i > rows/3.05 and i < (2*rows/3) and j < cols/1) {
+				vec.at(i).at(j).at(RED) *= 1.5;
+				vec.at(i).at(j).at(GREEN) *= 1.5;
+				vec.at(i).at(j).at(BLUE) *= 0.7;
+				}
+
 
 			
 
